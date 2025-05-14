3bf6092481340997079d1946dddd9f30ddf1e394
chelseanada <chelseac@sheridancollege.ca>
2025-05-14 13:26:46 -0400

    code fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: '#00d1b2',
-          secondary: '#ffffff',
-          muted: '#a1a1a1',
-          background: '#0a0a0a',
-          card: '#1c1c1c',
-          btnHover: '#00b3a4',
-        },
-        fontFamily: {
-          sans: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
-        },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gradient: "hsla(176, 45%, 66%, 1)",
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
